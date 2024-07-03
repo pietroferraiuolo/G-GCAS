757f3cc5d5e1d6ffc01cfbe91d5c1c0330aecf97
pietroferraiuolo <pietro.ferraiuolo@inaf.it>
2024-07-03 21:39:36 +0200

    shpinx pt2

--- untitled0.py
+++
@@ -1,6 +0,0 @@
-from ggcas import plots as gplt
-import numpy as np
-x = np.random.randn(1000)
-z = np.random.randn(1000)
-gplt.scat_xhist(x, z)
-gplt.scat_xhist(x, z, xerr=0.051, xlabel=r'$v_{los}$', ylabel=r'$R$', color='red')
