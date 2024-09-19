014cd084e9dba0baa03c557f0fd727ff9be589b5
pietroferraiuolo <pietro.ferraiuolo@inaf.it>
2024-09-19 16:37:32 +0200

    Refactor test_folder_paths.py for improved testability and maintainability

--- test/utility/test_folder_paths.py
+++ test/utility/test_folder_paths.py
@@ -1,20 +1,16 @@
-import unittest
+import unittest, mock
+from unittest.mock import patch
 import os
-
-# Save original environment variable
-original_gcasdata = os.getenv('GCASDATA')
-# Set up the environment variable for testing before importing the module
-os.environ['GCASDATA'] = '/mock/path/to/G-GCAS/ggcas'
-
-# Now import the module
-from ggcas.utility.folder_paths import CLUSTER_DATA_FOLDER, CLUSTER_MODEL_FOLDER
+from ggcas.utility import folder_paths as fn
 
 class TestFolderPaths(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
         # Save the original environment variable if it exists
-        cls.original_gcasdata = original_gcasdata
+        cls.original_gcasdata = os.getenv('GCASDATA')
+        # Set up the environment variable for testing
+        os.environ['GCASDATA'] = '/mock/path/to/G-GCAS/ggcas'
 
     @classmethod
     def tearDownClass(cls):
@@ -24,18 +20,30 @@ class TestFolderPaths(unittest.TestCase):
         else:
             del os.environ['GCASDATA']
 
-    def test_gcas_data_folder(self):
-        self.assertEqual(os.getenv('GCASDATA'), '/mock/path/to/G-GCAS/ggcas')
+    def setUp(self):
+        #Reset the environment variable after each test
+        os.environ['GCASDATA'] = '/mock/path/to/G-GCAS/ggcas'
 
-    def test_cluster_data_folder(self):
+    @patch('os.getenv', return_value='/mock/path/to/G-GCAS/ggcas')
+    @patch('ggcas.utility.folder_paths.BASE_PATH', return_value='os.getenv("GCASDATA")')
+    def test_gcas_data_folder(self, mock_getenv, mock_BASE_PATH):
+        self.setUp()
+        self.assertEqual(fn.BASE_DATA_PATH, '/mock/path/to/G-GCAS/ggcas/data')
+        self.assertEqual(fn.QUERY_DATA_FOLDER, '/mock/path/to/G-GCAS/ggcas/data/query')
+
+    @patch('os.getenv', return_value='/mock/path/to/G-GCAS/ggcas')
+    @patch('ggcas.utility.folder_paths.BASE_PATH', return_value='os.getenv("GCASDATA")')
+    def test_cluster_data_folder(self, mock_getenv, mock_BASE_PATH):
         expected_path = '/mock/path/to/G-GCAS/ggcas/data/query/CLUSTER1'
-        actual_path = CLUSTER_DATA_FOLDER('cluster1')
+        actual_path = fn.CLUSTER_DATA_FOLDER('cluster1')
         print(f"Expected: {expected_path}, Actual: {actual_path}")
         self.assertEqual(actual_path, expected_path)
 
-    def test_cluster_model_folder(self):
+    @patch('os.getenv', return_value='/mock/path/to/G-GCAS/ggcas')
+    @patch('ggcas.utility.folder_paths.BASE_PATH', return_value='os.getenv("GCASDATA")')
+    def test_cluster_model_folder(self, mock_getenv, mock_BASE_PATH):
         expected_path = '/mock/path/to/G-GCAS/ggcas/data/models/CLUSTER1'
-        actual_path = CLUSTER_MODEL_FOLDER('cluster1')
+        actual_path = fn.CLUSTER_MODEL_FOLDER('cluster1')
         print(f"Expected: {expected_path}, Actual: {actual_path}")
         self.assertEqual(actual_path, expected_path)
 
