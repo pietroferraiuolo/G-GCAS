8e0919680becd8b963e1371b33d5fcf6d6fcb492
pietroferraiuolo <pietro.ferraiuolo@inaf.it>
2024-10-08 10:45:11 +0200

    Refactor statistics.py module and update imports

--- ggcas/statistics.py
+++ ggcas/statistics.py
@@ -10,35 +10,13 @@ Description
 import time
 import numpy as np
 import pandas as pd
-import emcee
-import pymc as pm
-from astroML import XDGMM
-from sklearn.mixture import (
-    BayesianGaussianMixture,
-    GaussianMixture
+import rpy2.robjects as ro
+from rpy2.robjects import (
+    pandas2ri as p2r,
+    numpy2ri as n2r,
+    r as R,
+    globalenv as genv
 )
-from sklearn.metrics import (
-    classification_report as cr,
-    confusion_matrix as cm,
-    accuracy_score as acs
-)
-from sklearn.model_selection import train_test_split as tts
-
-def gaussianMixture(data, *args, **kwargs):
-    rs = _seed()
-    gm = GaussianMixture(*args, **kwargs)
-    train_data, temp_data = tts(data, train_size=0.9, random_state=rs)
-    validation_data, test_data = tts(temp_data, test_size=0.5, random_state=rs)
-    gm.fit(train_data)
-    return gm, validation_data, test_data
-
-def bayesianGaussianMixture(data, *args, **kwargs):
-    rs = _seed()
-    gm = BayesianGaussianMixture(*args, **kwargs)
-    train_data, temp_data = tts(data, train_size=0.9, random_state=rs)
-    validation_data, test_data = tts(temp_data, test_size=0.5, random_state=rs)
-    gm.fit(train_data)
-    return gm, validation_data, test_data
 
 def _seed():
     return int(time.time())
